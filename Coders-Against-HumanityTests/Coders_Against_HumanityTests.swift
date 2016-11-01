8e70ac819d50aa0d02d46ff054ed5b151ff52b9e
Ji Kim <jkim55@gmail.com>
2016-11-01 10:47:49 -0600

    Initial Commit

---
+++ Coders-Against-HumanityTests/Coders_Against_HumanityTests.swift
@@ -0,0 +1,36 @@
+//
+//  Coders_Against_HumanityTests.swift
+//  Coders-Against-HumanityTests
+//
+//  Created by Ji Kim on 11/1/16.
+//  Copyright © 2016 Ji. All rights reserved.
+//
+
+import XCTest
+@testable import Coders_Against_Humanity
+
+class Coders_Against_HumanityTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
