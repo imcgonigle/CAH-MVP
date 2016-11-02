4aa4c5371b9ea92044127bca8d0aaada1840abe9
Ji Kim <jkim55@gmail.com>
2016-11-01 19:00:10 -0600

    refactored & cleaned up comments

--- Coders-Against-Humanity/VoteVC.swift
+++ Coders-Against-Humanity/VoteVC.swift
@@ -2,8 +2,8 @@
 //  VoteVC.swift
 //  Coders-Against-Humanity
 //
-//  Created by Ji Kim on 11/1/16.
-//  Copyright © 2016 Ji. All rights reserved.
+//  Created by ThunderWhatever on 11/1/16.
+//  Copyright © 2016 ThunderWhatever. All rights
 //
 
 import UIKit
@@ -12,24 +12,12 @@ class VoteVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.black
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+// END OF CLASS: VoteVC
 }
--- Coders-Against-Humanity/WinnerVC.swift
+++ Coders-Against-Humanity/WinnerVC.swift
@@ -2,8 +2,8 @@
 //  WinnerVC.swift
 //  Coders-Against-Humanity
 //
-//  Created by Ji Kim on 11/1/16.
-//  Copyright © 2016 Ji. All rights reserved.
+//  Created by ThunderWhatever on 11/1/16.
+//  Copyright © 2016 ThunderWhatever. All rights
 //
 
 import UIKit
@@ -12,24 +12,11 @@ class WinnerVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+// END OF CLASS: LoginVC
 }
